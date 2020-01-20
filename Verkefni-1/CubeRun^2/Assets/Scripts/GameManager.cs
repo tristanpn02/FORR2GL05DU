da141e2822aab0476f39b347f422642544570d39
Tristan Andersen <tristanpn02@gmail.com>
2020-01-20 18:19:57 +0000

    Create CubeRun^2

---
+++ Verkefni-1/CubeRun^2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        Debug.Log("Level won!");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame ()
+    {
+        if (gameEnded == false) { 
+            gameEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Verkefni-1/CubeRun^2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Reference to RigidBody component
+    public Rigidbody rb;
+
+    // Force variables
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Marked as "FixedUpdate" since physics is being manipulated
+    void FixedUpdate() {
+        // Add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -2f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
